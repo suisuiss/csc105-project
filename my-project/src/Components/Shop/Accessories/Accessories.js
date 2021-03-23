d045ed4b94e0074c839044bb360459fec8305ecd
Thanawan <thanawan.ss@mail.kmutt.ac.th>
2021-03-23 23:58:58 +0700

    my-project

--- my-project/src/Components/Shop/Accessories/Accessories.js
+++ my-project/src/Components/Shop/Accessories/Accessories.js
@@ -3,80 +3,81 @@ import './Accessories.css'
 
 export const Accessories = () => {
     return (
-        
-            <div>
+
+        <div>
             <h1 id="accessories">Accessories</h1>
             <div id="accessories1">
-            <img src="https://www.haglofs.com/dw/image/v2/BBTN_PRD/on/demandware.static/-/Sites-master-catalog-haglofs/default/dw2674d8dc/hi-res/6043062C5_S20_2_grey.jpg?sw=350&sh=350&sm=fit"></img>
+                <img src="https://www.haglofs.com/dw/image/v2/BBTN_PRD/on/demandware.static/-/Sites-master-catalog-haglofs/default/dw2674d8dc/hi-res/6043062C5_S20_2_grey.jpg?sw=350&sh=350&sm=fit"></img>
                 <h1>239 Baht</h1>
                 <p>A sun hat with a brim - perfect for protection from a number of adverse weather conditions.</p>
                 <a href="" id="details1">see more details</a>
                 <a href="" id="cart1">Add To Shopping Cart</a>
             </div>
             <div id="accessories2">
-            <img src="https://static.zara.net/photos///2020/I/1/1/p/1202/610/050/2/w/1920/1202610050_1_1_1.jpg?ts=1597059771480"></img>
+                <img src="https://static.zara.net/photos///2020/I/1/1/p/1202/610/050/2/w/1920/1202610050_1_1_1.jpg?ts=1597059771480"></img>
                 <h1>299 Baht</h1>
                 <p>ASYMMETRIC LEATHER HIGH-HEEL SHOES </p>
                 <a href="" id="details2">see more details</a>
                 <a href="" id="cart2">Add To Shopping Cart</a>
             </div>
             <div id="accessories3">
-            <img src="https://m.media-amazon.com/images/I/51uSGJGdtAL._AC_SX385._SX._UX._SY._UY_.jpg"></img>
-                <h1>1380 Baht</h1>
-                <p>Warm Trench Parka,Women's Winter Coat Jacket Belt Overcoat Changeshopping</p>
+                <img src="https://www.bodenimages.com/productimages/zoom/20wwin_a0807_nav.jpg"></img>
+                <h1>329 Baht</h1>
+                <p>Wool Scarf - Navy</p>
                 <a href="" id="details3">see more details</a>
                 <a href="" id="cart3">Add To Shopping Cart</a>
             </div>
             <div id="accessories4">
-            <img src="https://cdn.modesens.com/media/93099291"></img>
-                <h1>2239 Baht</h1>
-                <p>Bear Women Hoodie In Off White</p>
+                <img src="https://i.pinimg.com/originals/11/72/50/117250d6159229d4ab722c3ed3b690fc.jpg"></img>
+                <h1>459 Baht</h1>
+                <p>Retro Round Polarized Sunglasses Silver Metal Circle Frame Gray Lens Circular</p>
                 <a href="" id="details4">see more details</a>
                 <a href="" id="cart4">Add To Shopping Cart</a>
             </div>
             <div id="accessories5">
-            <img src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fb2%2Ffd%2Fb2fd220fe1608a9fb3f266e205cb0919e13e6399.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bladies_tops_shortsleeve%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url[file:/product/main]"></img>
-                <h1>379 Baht</h1>
-                <p>Woman T-shirt in lightweight jersey with a rounded hem. Slightly longer at the back.</p>
+                <img src="https://asset.swarovski.com/images/$size_1450/t_swa103/b_rgb:ffffff,c_scale,dpr_3.0,f_auto,w_500/5469989_png/dazzling-swan-necklace--multi-colored--rose-gold-tone-plated-swarovski-5469989.png"></img>
+                <h1>639 Baht</h1>
+                <p>Woodworker's Necktie</p>
                 <a href="" id="details5">see more details</a>
                 <a href="" id="cart5">Add To Shopping Cart</a>
             </div>
             <div id="accessories6">
-            <img src="https://sc01.alicdn.com/kf/HTB1F0ZlPVXXXXb7XFXXq6xXFXXXS/220915899/HTB1F0ZlPVXXXXb7XFXXq6xXFXXXS.jpg_.webp"></img>
-                <h1>2170 Baht</h1>
-                <p>Pakistani Women Dresses, Long Sleeve Midi Dress Women's Vintage Polka Dot Optical Illusion Bridesmaid Party Causal Dress</p>
+                <img src="https://earthseasky.co.nz/wp-content/uploads/2011/10/polypro_gloves.jpg"></img>
+                <h1>199 Baht</h1>
+                <p>First Layer gloves are ideal for providing a layer of warmth and insulation for your hands.</p>
                 <a href="" id="details6">see more details</a>
                 <a href="" id="cart6">Add To Shopping Cart</a>
             </div>
             <div id="accessories7">
-            <img src="https://s7.landsend.com/is/image/LandsEnd/395789_BP10_LF_BLA?$210x315$"></img>
-                <h1>390 Baht</h1>
-                <p>Women's Long Sleeve Essential T-shirt</p>
+                <img src="https://www.rei.com/media/fb51e06c-8b92-406f-8803-6fb2c84ada00?size=784x588"></img>
+                <h1>330 Baht</h1>
+                <p>Injinji Liner Crew Socks</p>
                 <a href="" id="details7">see more details</a>
                 <a href="" id="cart7">Add To Shopping Cart</a>
             </div>
             <div id="accessories8">
-            <img src="https://scene7.zumiez.com/is/image/zumiez/cat_max/Dickies-Lilac-Roll-Hem-Shorts-_341238.jpg"></img>
-                <h1>539 Baht</h1>
-                <p>Dickies Lilac Roll Hem Shorts</p>
+                <img src="https://www.chloe.com/product_image/11743517BJ/f/w282.jpg"></img>
+                <h1>514 Baht</h1>
+                <p>BETTY RAIN BOOT PVC Black </p>
                 <a href="" id="details8">see more details</a>
                 <a href="" id="cart8">Add To Shopping Cart</a>
             </div>
             <div id="accessories9">
-            <img src="https://www.maisonmargiela.com/13/13456301hd_14_f.jpg"></img>
-                <h1>1800 Baht</h1>
-                <p>Shadow 5-pocket jeans</p>
+                <img src="https://media.beaverbrooks.co.uk/i/beaverbrooks/S112750_0/Silver-Rose-Gold-Plated-and-Gold-Plated-Feather-Drop-Earrings-0112750?$PDP_M$"></img>
+                <h1>517 Baht</h1>
+                <p>Silver, Rose Gold Plated and Gold Plated Feather Drop Earrings</p>
                 <a href="" id="details9">see more details</a>
                 <a href="" id="cart9">Add To Shopping Cart</a>
             </div>
             <div id="menclothes10">
-            <img src="https://i.pinimg.com/originals/bb/08/c8/bb08c8877de8e055b4ee4c978d7d55ee.jpg"></img>
-                <h1>779 Baht</h1>
-                <p>Stylish V-Neck Half Sleeve Solid Color Blouse For Women</p>
+                <img src="https://rukminim1.flixcart.com/image/880/1056/k0plpjk0/shoe/8/a/t/8908723-42-roadster-navy-blue-original-imafk9wchnsnfggp.jpeg?q=50"></img>
+                <h1>2199 Baht</h1>
+                <p>ROADSTER
+                Sneakers For Men  (Navy)</p>
                 <a href="" id="details10">see more details</a>
                 <a href="" id="cart10">Add To Shopping Cart</a>
             </div>
         </div>
-        )
-    
+    )
+
 }
