72cc3f9c41c23f0048fc60f3028108b37e1c0088
Thanawan <thanawan.ss@mail.kmutt.ac.th>
2021-03-23 16:39:27 +0700

    my-project

--- my-project/src/Components/Product/Product.js
+++ my-project/src/Components/Product/Product.js
@@ -5,7 +5,11 @@ export const Product = () => {
         <div>
         <h2 id="recommend">Recommended Product</h2>
         <div id="product1">
-            
+         <img src="https://dj7u9rvtp3yka.cloudfront.net/products/PIM-1546095976599-a1785a3b-a36a-491d-afe9-ad631d366262_v1-small.jpg"></img> 
+         <h1>599 Baht</h1>
+         <p>Men Shirt Slim Fit Design Solid Color Long Sleeve Formal Casual Business Male Clothing</p>  
+        <a href="" id="details">see more details</a>
+        <a href="" id="cart">Add To Shopping Cart</a>
         </div>
         <div id="product2">
             
