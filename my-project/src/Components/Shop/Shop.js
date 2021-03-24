719dcf9b5d4963174a2e8fd88c5a1ac1111b9e46
bbibbibiq <peerasorn@hot-now.com>
2021-03-24 12:08:29 +0700

    Remove unused

--- my-project/src/Components/Shop/Shop.js
+++ my-project/src/Components/Shop/Shop.js
@@ -62,53 +62,6 @@ const Shop = () => {
           <Product />
         </div>
       </div>
-      {/* <div className="llogo">
-        <a className="dailydeal">
-          <h2>Daily Deal</h2>
-          <p>your best shopping centre</p>
-        </a>
-      </div>
-      <div className="search">
-        <input type="search" placeholder="What do you want to get?" />
-      </div>
-
-      <div className="category">
-        <h2>Your Options</h2>
-        <a className="menClothes" href="./Menclothes/Menclothes.js">
-          Men Clothes
-        </a>
-        <a className="womenClothes" href="./WomenClothes/WomenClothes.js">
-          Women Clothes
-        </a>
-        <a className="accessories" href="./Accessories/Accessories.js">
-          Accessories
-        </a>
-        <a className="electronic" href="./Gadgets/Gadgets.js">
-          Gadgets
-        </a>
-        <a className="snack" href="./Snacks/Snacks.js">
-          Snack
-        </a>
-      </div>
-      <div className="news" />
-      <div className="button-container">
-        <a href=".html">
-          <img src={shopcart} alt="Shop Cart" name="cart" />
-        </a>
-        <div className="bell-container">
-          <a href="">
-            <img src={bell} alt="Notifications" name="bell" />
-          </a>
-        </div>
-        <a href=".html">
-          <img src={setting} alt="Setting" name="setting" />
-        </a>
-        <a href=".html">
-          <img src={user} alt="User" name="user" />
-        </a>
-      </div> */}
-
-      {/* <Product /> */}
     </div>
   );
 };
