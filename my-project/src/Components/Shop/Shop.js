2b2cbb55bdd88cf0d75898cf4cd5e45658b0f9d8
bbibbibiq <peerasorn@hot-now.com>
2021-03-24 12:35:52 +0700

    Can show notification

--- my-project/src/Components/Shop/Shop.js
+++ my-project/src/Components/Shop/Shop.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./shop.css";
 import bell from "./bell.png";
 import shopcart from "./shop cart.png";
@@ -6,7 +6,60 @@ import setting from "./setting.png";
 import user from "./user.png";
 import { Product } from "../Product/Product";
 
+const notifications = [
+  {
+    image:
+      "https://dj7u9rvtp3yka.cloudfront.net/products/PIM-1546095976599-a1785a3b-a36a-491d-afe9-ad631d366262_v1-small.jpg",
+    description: "Menclothes Sale of 50%",
+  },
+  {
+    image:
+      "https://www.haglofs.com/dw/image/v2/BBTN_PRD/on/demandware.static/-/Sites-master-catalog-haglofs/default/dw2674d8dc/hi-res/6043062C5_S20_2_grey.jpg?sw=350&sh=350&sm=fit",
+    description: "Accessories Sale of 10%",
+  },
+  {
+    image:
+      "https://static.zara.net/photos///2020/I/1/1/p/1202/610/050/2/w/1920/1202610050_1_1_1.jpg?ts=1597059771480",
+    description: "Louboutin Buy 1 get 1 Free!",
+  },
+];
+
+const NotificationContainer = () => {
+  return (
+    <div className="notification">
+      {notifications.map((n) => NotificationItem(n))}
+    </div>
+  );
+};
+
+const NotificationItem = ({ image, description }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        borderBottom: "1px solid black",
+        padding: "10px 0",
+      }}
+    >
+      <img style={{ width: "1.5rem", height: "1.5rem" }} src={image} />
+      <span
+        style={{
+          fontSize: "13px",
+          alignSelf: "center",
+          margin: "0 10px",
+          display: "inline-block",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+        }}
+      >
+        {description}
+      </span>
+    </div>
+  );
+};
+
 const Shop = () => {
+  const [isShowNotification, setIsShowNotification] = useState(false);
   return (
     <div className="container">
       <div className="header">
@@ -23,9 +76,14 @@ const Shop = () => {
               <img src={shopcart} alt="Shop Cart" name="cart" />
             </a>
             <div className="bell-container">
-              <a href="">
-                <img src={bell} alt="Notifications" name="bell" />
-              </a>
+              <img
+                src={bell}
+                style={{ cursor: "pointer" }}
+                alt="Notifications"
+                name="bell"
+                onClick={() => setIsShowNotification(!isShowNotification)}
+              />
+              {isShowNotification ? <NotificationContainer /> : <div></div>}
             </div>
             <a href=".html">
               <img src={setting} alt="Setting" name="setting" />
